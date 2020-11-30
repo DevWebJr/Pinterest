08bd156484cee654808c4fa7e1c1e0f371408763
DevWebJr <devwebjr@gmail.com>
2020-11-30 01:38:53 +0100

    add validation by @Assert

--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Entity\Traits\TimeStampable;
 use App\Repository\PinRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=PinRepository::class)
@@ -23,11 +24,15 @@ class Pin
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Title can not be blank")
+     * @Assert\Length(min=3, minMessage="3 caracters minimum")
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank
+     * @Assert\Length(min=10, minMessage="10 caracters minimum")
      */
     private $description;
 
